794ef6ff2df3f658b297aa1d974c3f3019d4bf0f
Luan Luz <luanbrunoalvesluz@gmail.com>
2025-11-16 13:41:27 -0300

    feat(repository): implementa repositories para assunto, autor e livro
    - cria `assunto repository` para gerenciar operações de persistência da entidade `assunto`.
    - Cria `autor repository` para gerenciar operações de persistência da entidade `autor`.
    - cria `livro repository` para gerenciar operações de persistência da entidade `livro`.
    - adiciona métodos `exists by ignore case` para verificar a existência de entidades por nome/descrição.
    
    closes #3

---
+++ src/main/java/biblioteca/dev/luanluz/api/repository/AssuntoRepository.java
@@ -0,0 +1,11 @@
+package biblioteca.dev.luanluz.api.repository;
+
+import biblioteca.dev.luanluz.api.model.Assunto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssuntoRepository extends JpaRepository<Assunto, Integer> {
+
+    boolean existsByDescricaoIgnoreCase(String descricao);
+}
---
+++ src/main/java/biblioteca/dev/luanluz/api/repository/AutorRepository.java
@@ -0,0 +1,11 @@
+package biblioteca.dev.luanluz.api.repository;
+
+import biblioteca.dev.luanluz.api.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Integer> {
+
+    boolean existsByNomeIgnoreCase(String nome);
+}
