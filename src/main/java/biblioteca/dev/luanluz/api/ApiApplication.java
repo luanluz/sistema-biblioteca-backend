67c2decaa187e189ce794abfe524b5139b57376c
luan <40572816+luanluz@users.noreply.github.com>
2025-11-17 07:36:48 -0300

    fix(config): configura swagger e forward headers para deploy em proxy (#40)
    - adiciona anotação `open api definition` para configurar a url do swagger com base no `server.servlet.context-path`.
    - Adiciona `server.forward-headers-strategy=framework` para habilitar o uso de cabeçalhos encaminhados por proxies.
    
    closes #39

--- src/main/java/biblioteca/dev/luanluz/api/ApiApplication.java
+++ src/main/java/biblioteca/dev/luanluz/api/ApiApplication.java
@@ -1,8 +1,11 @@
 package biblioteca.dev.luanluz.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "${server.servlet.context-path}", description = "Default Server URL")})
 @SpringBootApplication
 public class ApiApplication {
 
