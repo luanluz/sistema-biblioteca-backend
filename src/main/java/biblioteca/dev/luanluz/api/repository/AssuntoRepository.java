8e346019dd6b7d4427d5f820cedcf1fe730759a4
Luan Luz <luanbrunoalvesluz@gmail.com>
2025-11-17 04:04:33 -0300

    feat: implementa endpoint de assuntos
    - cria `assunto controller` com endpoints de crud.
    - cria dto de paginação de assunto.
    - adiciona documentação open api para os endpoints de assunto.
    - refatora a validação de descrição única no `assunto service` e `assunto repository`.
    
    closes #8

--- src/main/java/biblioteca/dev/luanluz/api/repository/AssuntoRepository.java
+++ src/main/java/biblioteca/dev/luanluz/api/repository/AssuntoRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface AssuntoRepository extends JpaRepository<Assunto, Integer> {
 
     boolean existsByDescricaoIgnoreCase(String descricao);
+
+    boolean existsByDescricaoIgnoreCaseAndCodigoNot(String descricao, Integer codigo);
 }
