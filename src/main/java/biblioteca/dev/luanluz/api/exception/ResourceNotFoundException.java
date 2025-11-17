77655e42513d2532272f60d3b5f4ff377b1c6c80
Luan Luz <luanbrunoalvesluz@gmail.com>
2025-11-16 23:08:34 -0300

    feat(exception): cria exceções de domínio personalizadas
    - cria `domain exception` para representar erros de regras de negócio.
    - cria `resource not found exception` para representar um recurso não é encontrado.
    - cria `duplicate resource exception` para tratar violações de recursos únicos.
    
    closes #4

---
+++ src/main/java/biblioteca/dev/luanluz/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package biblioteca.dev.luanluz.api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resource, String field, Object value) {
+        super(String.format("%s não encontrado(a) com %s: '%s'", resource, field, value));
+    }
+}
